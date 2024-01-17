c8496cd1dff50bf62c9101f61f81a12c391c27ba
W.G. Kasun Dimantha <kasundimantha97@gmail.com>
2024-01-17 08:10:30 +0530

    Connect Database

---
+++ Sample_Supermarket_BillSystem/src/edu/mvc/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.mvc;
+
+/**
+ *
+ * @author tharidu dilshan
+ */
+public class Main {
+    public static void main(String[] args) {
+       // new CustomerView().setVisible(true);
+       // new ItemView().setVisible(true);
+        //new OrderView().setVisible(true);
+        System.out.println("hello");
+    }
+}
